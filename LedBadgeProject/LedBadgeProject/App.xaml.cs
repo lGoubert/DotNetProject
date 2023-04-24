170ea4b23c5d5a6cc9ceddd25068d50ba79bdb5e
Lucas Goubert <lgoubert@groupeoptimis.fr>
2023-04-24 09:04:58 +0200

    New project

--- LedBadgeProject/LedBadgeProject/App.xaml.cs
+++ LedBadgeProject/LedBadgeProject/App.xaml.cs
@@ -6,7 +6,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace LedBadge
+namespace LedBadgeProject
 {
     /// <summary>
     /// Interaction logic for App.xaml
